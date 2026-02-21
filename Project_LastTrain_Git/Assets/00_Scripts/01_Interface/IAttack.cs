08e5d51f1250379a86178419428c53d1ae2be5d0
programjhlee <programjhlee@gmail.com>
2026-02-21 14:43:32 +0900

    Fix branch Error

--- Project_LastTrain_Git/Assets/00_Scripts/01_Interface/IAttack.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IAttack 
-{
-    public void Attack(IAttackable _attackable,Vector3 attackDir);
-}
